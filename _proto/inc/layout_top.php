934b022802702b784fcaaae7445a7d4868422baf
jbw <qzkml3@gmail.com>
2018-08-04 06:04:40 +0900

    오잉

--- _proto/inc/layout_top.php
+++ _proto/inc/layout_top.php
@@ -2,7 +2,10 @@
 <html lang="ko">
 <head>
 	<title><?=DOC_TITLE?> : <?=SITE_TITLE?></title>
+
 	<meta charset="UTF-8">
+
+    <link rel="stylesheet" href="/_proto/css/util.css">
 </head>
 <body>
 layout_top : proto
