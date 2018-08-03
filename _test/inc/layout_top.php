c04716c4830f65da74ad88941f4d14cf89171bc3
qzkml3 <qzkml3@gmail.com>
2018-08-03 09:44:10 +0900

    test

--- _test/inc/layout_top.php
+++ _test/inc/layout_top.php
@@ -2,8 +2,13 @@
 <html lang="ko">
 <head>
 	<title><?=DOC_TITLE?> : <?=SITE_TITLE?></title>
+
 	<meta charset="UTF-8">
+
+	<link rel="stylesheet" href="/_test/css/test.css">
+
+	<script src="/_proto/js/jquery/jquery-1.8.3.min.js"></script>
 </head>
 <body>
-layout_top : <?=SITE_TITLE?>
-<br>
+	layout_top : <?=SITE_TITLE?>
+	<br>
