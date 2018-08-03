7b3910d1e1df7915f7d21c4fe81b681afa6933c3
qzkml3 <qzkml3@gmail.com>
2018-08-03 10:19:28 +0900

    _proto

--- _proto/inc/layout_top.php
+++ _proto/inc/layout_top.php
@@ -4,6 +4,7 @@
 	<title><?=DOC_TITLE?> : <?=SITE_TITLE?></title>
 
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 	<script src="/_proto/js/jquery/jquery-1.8.3.min.js"></script>
 </head>
