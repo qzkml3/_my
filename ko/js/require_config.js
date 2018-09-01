9cb915cf69e79662c9df104935994c6f416bb91b
qzkml3 <qzkml3@gmail.com>
2018-09-02 04:03:37 +0900

    test > sub

--- ko/js/require_config.js
+++ ko/js/require_config.js
@@ -1,10 +1,14 @@
 require.config({
 	paths: {
 		"jquery": "/ko/js/jquery/jquery-1.8.3.min",
-		"common": "/ko/js/common"
+		"common": "/ko/js/common",
+		"UI": "/ko/js/class/UI"
 	},
 	shim: {
 		"common": {
+			deps: ["jquery", "UI"]
+		},
+		"UI": {
 			deps: ["jquery"]
 		}
 	}
