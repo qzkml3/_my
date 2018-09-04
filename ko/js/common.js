fae2a0c2a54009541bab1c314c56680b34757890
qzkml3 <qzkml3@gmail.com>
2018-09-04 12:59:11 +0900

    test > sub

--- ko/js/common.js
+++ ko/js/common.js
@@ -1,1 +1,42 @@
 console.log("loaded: common");
+
+System = {
+	getDebugMode: function() {
+		return true;
+	}
+};
+
+Browser = {
+	getIE: function() {
+
+	},
+	getChrome: function() {
+
+	},
+	getFireFox: function() {
+
+	},
+	getIPhone:  function() {
+
+	}
+}
+
+
+UI = {
+	tab: function (tabBtn, tabCont) {
+		var $tabBtn = $(tabBtn);
+		var $tabCont = $(tabCont);
+
+		$tabBtn.each(function (i) {
+			$(this).on("click", function () {
+				$(".tabBtn").removeClass("on");
+				$(this).eq(i).addClass("on");
+
+				$tabCont.hide();
+				$tabCont.eq(i).show();
+
+				return false;
+			});
+		});
+	}
+};
